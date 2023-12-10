4b23cc2e6a0c4377175aafa718b5d88c0d8f9956
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-12-10 21:41:02 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const secondColumnCells = document.querySelectorAll('tbody td:nth-child(2)');
+
+const clonedCells = [];
+
+secondColumnCells.forEach((cell) => {
+  const clonedCell = cell.cloneNode(true);
+
+  clonedCells.push(clonedCell);
+});
+
+clonedCells.forEach((clonedCell) => {
+  document.querySelectorAll('tbody tr').forEach((row) => {
+    row.appendChild(clonedCell.cloneNode(true));
+  });
+});
+
+const tfootRow = document.querySelector('tfoot tr');
+
+clonedCells.forEach((clonedCell) => {
+  tfootRow.appendChild(clonedCell.cloneNode(true));
+});
